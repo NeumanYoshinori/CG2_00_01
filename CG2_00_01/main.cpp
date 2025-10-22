742d5d6e9e8e649e68723828a4d1573158e718a7
NeumanYoshinori <k024g0084@g.neec.ac.jp>
2025-10-23 01:12:52 +0900

    ファイルを整理 フィルターを整理

--- CG2_00_01/main.cpp
+++ CG2_00_01/main.cpp
@@ -12,7 +12,7 @@
 #include <strsafe.h>
 #include <dxgidebug.h>
 #include <dxcapi.h>
-#include "Matrix.h"
+#include "engine/math/Matrix.h"
 #include "externals/imgui/imgui.h"
 #include "externals/imgui/imgui_impl_dx12.h"
 #include "externals/imgui/imgui_impl_win32.h"
